29815ea638bb87044e9062426a2b391abf123371
CorpeAI <35112540+corpeai@users.noreply.github.com>
2025-02-22 14:02:42 -0500

    Update Logo.tsx

--- src/components/LandingPage/images/Logo.tsx
+++ src/components/LandingPage/images/Logo.tsx
@@ -1,9 +1,20 @@
 import { Box, Center, Flex, Text } from '@chakra-ui/react'
 
-import Logo from './images/logo.png'
+import { SvgIcon } from '@/icons/type'
 
-export default function Logo() {
+export default function Logo(props: SvgIcon) {
   const { width = 224, height = 60 } = props
 
-  )
-}
+  return (
+   <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="146pt" height="146pt" viewBox="0 0 224 60" style="" preserveAspectRatio="xMidYMid meet">
+<g fill="#FFFFFFFF" stroke="#FFFFFFFF">
+<path d="M 74.025 104.759 L 23.550 105.019 35.509 117.009 L 47.469 129.000 74.024 129.000 L 100.580 129.000 112.540 116.750 L 124.500 104.500 74.025 104.759 "/></g>
+<g fill="#FFFFFFFF" stroke="#FFFFFFFF">
+<path d="M 36.750 29.183 C 30.288 35.730 25.000 41.180 25.000 41.294 C 25.000 41.407 47.402 41.396 74.783 41.269 L 124.566 41.037 112.519 29.019 L 100.471 17.000 74.486 17.139 L 48.500 17.277 36.750 29.183 "/></g>
+<g fill="#FFFFFFFF" stroke="#FFFFFFFF">
+<path d="M 30.900 88.513 L 21.299 98.000 41.248 98.000 L 61.197 98.000 51.526 88.500 C 46.207 83.275 41.550 79.006 41.177 79.013 C 40.805 79.020 36.180 83.295 30.900 88.513 "/></g>
+<g fill="#FFFFFFFF" stroke="#FFFFFFFF">
+<path d="M 30.950 57.500 C 36.268 62.725 40.897 67.000 41.237 67.000 C 41.576 67.000 46.207 62.725 51.526 57.500 L 61.197 48.000 41.239 48.000 L 21.280 48.000 30.950 57.500 "/></g>
+<g fill="#FF0000FF" stroke="#FF0000FF">
+<path d="M 23.652 69.572 L 20.803 73.000 23.652 76.428 C 25.218 78.313 26.793 79.888 27.150 79.928 C 27.508 79.968 29.375 78.425 31.300 76.500 L 34.800 73.000 31.300 69.500 C 29.375 67.575 27.508 66.032 27.150 66.072 C 26.793 66.112 25.218 67.687 23.652 69.572 "/></g>
+</svg>
