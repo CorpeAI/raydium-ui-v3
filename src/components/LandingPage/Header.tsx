bc298a83645840aa4b54517e4e7ada1f683b80f9
CorpeAI <35112540+corpeai@users.noreply.github.com>
2025-02-22 04:52:39 -0500

    Update Header.tsx

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -5,7 +5,7 @@ import useScroll from '@/hooks/useScroll'
 
 import Button from '../Button'
 
-import Logo from './images/Logo'
+import Logo from './public/Logo'
 import LogoLegacy from './images/LogoLegacy'
 
 export default function Header() {
